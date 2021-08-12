ab9aa13de3491d60f662413f81a048da17692897
rafeeuadebara <adebararafeeu@yahoo.com>
2021-08-12 18:04:09 +0100

    worked on header component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css";
+import { createStore } from "redux";
+
+//STORE - globilised state
+
+//ACTION - increment for example
+
+//Reducer -
+
+// dispatch
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
